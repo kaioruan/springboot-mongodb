27044ef3aca0b69f182d365c6b7fcc8b48afe297
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-03 13:37:29 -0300

    feat: create route post users to insert in database

--- src/main/java/com/workshop/demo/services/UserService.java
+++ src/main/java/com/workshop/demo/services/UserService.java
@@ -2,10 +2,12 @@ package com.workshop.demo.services;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.demo.domain.User;
+import com.workshop.demo.dto.UserDTO;
 import com.workshop.demo.repository.UserRepository;
 import com.workshop.demo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
