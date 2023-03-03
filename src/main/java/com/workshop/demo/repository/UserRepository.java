1fabe2858235330035cf6e572b2e603ee82233ef
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-03 02:21:27 -0300

    feat: connect mongodb to repository and service files

---
+++ src/main/java/com/workshop/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.workshop.demo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.workshop.demo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/workshop/demo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.workshop.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.demo.domain.User;
+import com.workshop.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
