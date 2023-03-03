d9b7899b5c6835d1728e1389be425c5b806ee18f
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-03 17:17:46 -0300

    feat: create endpoint to return findById router /posts

---
+++ src/main/java/com/workshop/demo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.workshop.demo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.workshop.demo.domain.Post;
+import com.workshop.demo.repository.PostRepository;
+import com.workshop.demo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
