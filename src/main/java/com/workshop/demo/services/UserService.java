271c455b5fd8b5ad9403d1370cef552b15d0de15
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-03 13:16:59 -0300

    feat: create router FindById and implement custom error to NotFoundID

--- src/main/java/com/workshop/demo/services/UserService.java
+++ src/main/java/com/workshop/demo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.workshop.demo.services;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.demo.domain.User;
 import com.workshop.demo.repository.UserRepository;
+import com.workshop.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/workshop/demo/services/exception/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.workshop.demo.services.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(
+				System.currentTimeMillis(),
+				status.value(),
+				"Não encontrado",
+				e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
