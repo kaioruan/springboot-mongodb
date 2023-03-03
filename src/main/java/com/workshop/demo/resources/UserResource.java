64b95870fb39ae8457178f7b42463f7daaca4c10
Kaio Ruan <kaio.ruan@souunit.com.br>
2023-03-03 17:13:15 -0300

    feat: create endpoint to return all posts of user

--- src/main/java/com/workshop/demo/resources/UserResource.java
+++ src/main/java/com/workshop/demo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.workshop.demo.domain.Post;
 import com.workshop.demo.domain.User;
 import com.workshop.demo.dto.UserDTO;
 import com.workshop.demo.services.UserService;
@@ -38,6 +39,12 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	@PostMapping
